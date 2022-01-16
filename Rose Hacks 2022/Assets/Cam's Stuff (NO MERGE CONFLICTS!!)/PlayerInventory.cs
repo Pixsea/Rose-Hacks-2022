8c3cc29b687737a10108bb9205249f1bcd082789
Cameron Ghaemmaghami <cghaemm@yahoo.com>
2022-01-15 19:24:16 -0800

    Added Inventory Backend Functionality
    Inventories are hard

---
+++ Rose Hacks 2022/Assets/Cam's Stuff (NO MERGE CONFLICTS!!)/PlayerInventory.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    // new organizzzzzzational structure
+    // Item has mono
+    // Dictionary<string name, [quantity and description]>
+    // when item is gathered, copies data into inventory dictionary
+
+    
+    private Dictionary<string, string> inventoryDescr = new Dictionary<string, string>();
+    private Dictionary<string, int> inventoryQuantity = new Dictionary<string, int>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // add starting items if any
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(inventoryQuantity);
+    }
+
+    public void addItem(Item item)
+    {
+        if(inventoryDescr.ContainsKey(item.getName()))
+        {
+            inventoryQuantity[item.getName()] += item.getQuantity();
+        }
+        else
+        {
+            inventoryDescr[item.getName()] = item.getDescription();
+            inventoryQuantity[item.getName()] = item.getQuantity();
+        }
+
+    }
+
+    public void removeItem(string name, int num)
+    {
+        if(inventoryDescr.ContainsKey(name))
+        {
+            if(num >= inventoryQuantity[name])
+            {
+                inventoryDescr.Remove(name);
+                inventoryQuantity.Remove(name);
+            }
+            else
+            {
+                inventoryQuantity[name] = inventoryQuantity[name] - num;
+            }
+        }
+    }
+
+
+
+    // Add function to check if player has enough ingredients to craft something
+
+
+}
